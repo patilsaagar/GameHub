94e9bd4c0579803420d2f2e7944d47cffe00ba37
sagar.patil <sagar.patil@thoughtworks.com>
2025-01-20 14:21:46 +0530

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
-import { Button } from "@/components/ui/button"
-import { HStack } from "@chakra-ui/react"
-import './App.css'
+import { Grid, GridItem, useBreakpointValue } from '@chakra-ui/react';
+import './App.css';
 
 function App() {
+  // Use the hook to check if the screen is large or not
+  const isLgScreen = useBreakpointValue({ base: false, lg: true });
+
   return (
-    <HStack>
-      <Button>Click me</Button>
-    </HStack>
-  )
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+
+      {isLgScreen && (
+        <GridItem area="aside" bg="blue">
+          Side
+        </GridItem>
+      )}
+      
+      <GridItem area="main" bg="gold">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
