31088d04ae281ac62b550f88bb80a4a4ee2d6f2f
sagar.patil <sagar.patil@thoughtworks.com>
2025-01-20 14:31:48 +0530

    Build a navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Grid, GridItem, useBreakpointValue } from '@chakra-ui/react';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   // Use the hook to check if the screen is large or not
@@ -12,12 +13,12 @@ function App() {
         lg: `"nav nav" "aside main"`,
       }}
     >
-      <GridItem area="nav" bg="coral">
-        Nav
+      <GridItem area="nav">
+        <NavBar/>
       </GridItem>
 
       {isLgScreen && (
-        <GridItem area="aside" bg="blue">
+        <GridItem area="aside">
           Side
         </GridItem>
       )}
